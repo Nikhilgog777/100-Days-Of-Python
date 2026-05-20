ee7670a9c61815131b7ad73d9aae10363b8e64d2
NIKHIL GOSWAMI <nikhilgog123@gmail.com>
2026-05-20 10:51:36 +0530

    Add files via upload

---
+++ day_01_hello_world/main.py
@@ -0,0 +1,11 @@
+#Using Single quotes
+print('Hello World')
+print('My Name is Nikhil Goswami')
+
+#Using Double quotes
+print("Hello World!")
+print("My Name is Nikhil Goswami")
+
+#Using Triple quotes
+print('''Hello World!
+My Name is Nikhil Goswami''')
