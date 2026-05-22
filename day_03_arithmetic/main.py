e0c1f126160c3f7bf08fdce3283d1ee36483c806
NIKHIL GOSWAMI <nikhilgog123@gmail.com>
2026-05-22 08:06:38 +0530

    Add files via upload

---
+++ day_03_arithmetic/main.py
@@ -0,0 +1,14 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+#Arithmetic function
+sum = a+b
+difference = a-b
+product = a*b
+quotient = a/b
+
+print(f'''The sum is {sum}.
+The difference is {difference}.
+The quotient is {quotient}.
+The product is {product}.'''
+)
