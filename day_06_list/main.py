77b5379e108e592484455e2605c87a13978992db
NIKHIL GOSWAMI <nikhilgog123@gmail.com>
2026-05-25 09:14:31 +0530

    Add files via upload

---
+++ day_06_list/main.py
@@ -0,0 +1,13 @@
+shopping_list = ["Apple","Banana","Milk","Bread"]
+
+shopping_list.append("Pulses") # Used to add a string
+print(shopping_list)
+
+shopping_list.remove("Apple")  # Used to remove a specific string from the list
+print(shopping_list)
+
+print(shopping_list[1])  # We can  use indecies to display specific items
+print(shopping_list[0:3])  # Slicing can also be used on lists
+
+shopping_list.sort()  # Used to sort the list
+print(shopping_list)
