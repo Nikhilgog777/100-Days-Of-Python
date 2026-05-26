6b427394164a213f47e7c365b14b9049142252de
NIKHIL GOSWAMI <nikhilgog123@gmail.com>
2026-05-26 09:13:20 +0530

    Add files via upload

---
+++ day_07_tuples/main.py
@@ -0,0 +1,6 @@
+#  Use a tuple to store days of the week. Print “Weekend” for Saturday/Sunday using a loop.
+
+days = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+
+for val in days[5:]:  # days[5: ] = ("Saturday","Sunday")
+    print("Weekend") 
